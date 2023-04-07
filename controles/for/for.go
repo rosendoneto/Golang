1a8efb263bc51f2da9fd13dac76d76b1b7422be7
RosendoN <rosendo.fontes@mercadolivre.com>
2023-04-07 17:07:13 -0300

    Estruturas de controle

---
+++ controles/for/for.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	for i <= 10 { // não tem While em go
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 0; i <= 20; i += 2 {
+		fmt.Printf("%d ", i)
+	}
+
+	fmt.Println("\nMisturado... ")
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Print("Par ")
+		} else {
+			fmt.Print("Ímpar ")
+		}
+	}
+
+}
---
+++ controles/switch1/switch.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func notaParaConceito(n float64) string {
+	var nota = int(n)
+	switch nota { // Go não usa break
+	case 10:
+		fallthrough // Segue para o próximo case
+	case 9:
+		return "A"
+	case 8, 7:
+		return "B"
+	case 6, 5:
+		return "C"
+	case 4, 3:
+		return "D"
+	case 2, 1, 0:
+		return "E"
+	default:
+		return "Nota inválida"
+	}
+}
+
+func main() {
+	fmt.Println(notaParaConceito(9.8))
+	fmt.Println(notaParaConceito(6.9))
+	fmt.Println(notaParaConceito(2.1))
+	fmt.Println(notaParaConceito(11))
+}
