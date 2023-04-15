eb0ef89c3d64b9e4ca72dd5879370043e8998e16
RosendoN <rosendo.fontes@mercadolivre.com>
2023-04-15 17:05:48 -0300

    Pacotes

---
+++ go.mod
@@ -0,0 +1,5 @@
+module cursogo
+
+go 1.19
+
+require github.com/rosendoneto/area v0.0.0-20230415183605-223e6422157b
---
+++ pacote/usandolib/usandolib.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	goarea "github.com/rosendoneto/area"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(4.0))
+}
